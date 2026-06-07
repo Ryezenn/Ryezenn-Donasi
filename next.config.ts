7fa9d1b30baf59b05a623cc3a49e4752c762f748
Zarukabot <zarukayaagami@gmail.com>
2026-06-08 05:22:58 +0700

    fix: penyelesaian masalah deploy vercel dan dynamic config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 import path from "path";
 
+const isVercel = process.env.VERCEL === "1";
+
 const nextConfig: NextConfig = {
   outputFileTracingRoot: process.cwd(),
   typescript: {
@@ -9,13 +11,16 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config) => {
-    config.resolve.modules = [
-      path.resolve(process.cwd(), "node_modules"),
-      "node_modules",
-    ];
-    return config;
-  },
+  // Hanya gunakan custom Webpack config di komputer lokal, abaikan di Vercel agar Turbopack berjalan lancar
+  ...(isVercel ? {} : {
+    webpack: (config) => {
+      config.resolve.modules = [
+        path.resolve(process.cwd(), "node_modules"),
+        "node_modules",
+      ];
+      return config;
+    },
+  })
 };
 
 export default nextConfig;
